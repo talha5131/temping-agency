500d4facc436705a3e8c71f9ad278fca04c51999
Muhammad Talha <talhamalik4147@gmail.com>
2020-08-12 19:57:16 +0500

    Aug 12, 2020 7:57

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -3,7 +3,7 @@
 
 <head>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="public/assets/css/style.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
 </head>
@@ -78,11 +78,11 @@
 		}
 
 		.nav-bottom-content {
-			margin: 0
+			margin: 0;
 		}
-		/* .mobile-li{
-			font-size: 20px;
-		} */
+		.mobile-li{
+			font-size: 12px !important;
+		}
 	}
 </style>
 
@@ -97,7 +97,7 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a href="index.php" class="logo"><img class="img-responsive" src="public/assets/images/logo.png" alt="logo"></a>
+				<a href="{{url('/')}}" class="logo"><img class="img-responsive" src="public/assets/images/logo.png" alt="logo"></a>
 			</div>
 			<div class="collapse navbar-collapse" id="myNavbar">
 				<ul class="nav navbar-nav navbar-left nav-top-content nav-top-left" style="margin-left: 4%">
@@ -105,25 +105,25 @@
 						<a href="#" style=" color: white;">CALL US <strong>+442030869080</strong></a>
 					</li>
 					<li class="">
-						<a href="cv-upload.php" style=" color: white;">Get A Quote</a>
+						<a href="cv-upload" style=" color: white;">Get A Quote</a>
 					</li>
 					<li class="">
-						<a href="cv-upload.php" style=" color: white;">Email Us</a>
+						<a href="cv-upload" style=" color: white;">Email Us</a>
 					</li>
 				</ul>
 				<ul class="nav navbar-nav navbar-right nav-top-right" style="margin-top:0.5%;margin-right: 5%">
 					<li class="">
-						<a href="employer.php" class="btn btn-primary cd-btn" style="margin-top: 10px; color: white; font-size: 11px">Employer</a>
+						<a href="employer" class="btn btn-primary cd-btn" style="margin-top: 10px; color: white; font-size: 11px">Employer</a>
 					</li>
 					<li class="">
-						<a href="job-seeker.php" class="btn btn-primary cd-btn" style="margin-top: 10px; margin-left: 5px; color: white; font-size: 11px">Job Seeker</a>
+						<a href="job-seeker" class="btn btn-primary cd-btn" style="margin-top: 10px; margin-left: 5px; color: white; font-size: 11px">Job Seeker</a>
 					</li>
 					<li class="">
-						<a href="cv-upload.php" class="btn btn-primary cd-btn" style="margin-top: 10px;
+						<a href="cv-upload" class="btn btn-primary cd-btn" style="margin-top: 10px;
 						margin-left: 5px; color: white; font-size: 11px">CV Upload</a>
 					</li>
 					<li class="">
-						<a href="post-a-job.php" class="btn btn-primary cd-btn" style="margin-top: 10px; margin-left: 5px; color: white; font-size: 11px">Post A Job</a>
+						<a href="post-a-job" class="btn btn-primary cd-btn" style="margin-top: 10px; margin-left: 5px; color: white; font-size: 11px">Post A Job</a>
 					</li>
 					<li class="">
 						<a href="#" class="btn btn-primary cd-btn" style="padding-right:15px; margin-top: 10px;
@@ -132,49 +132,49 @@
 
 				</ul>
 				<ul class="nav navbar-nav navbar-left nav-bottom-content">
-					<li class="active mobile-li"><a href="index.php">Home </a></li>
-					<li><a href="about2.php">About Us</a></li>
-					<li><a href="why-us.php">Why Us</a></li>
-					<li><a href="career.php">Jobs</a></li>
+					<li class="active"><a href="{{url('/')}}" class="mobile-li">Home </a></li>
+					<li><a href="about2">About Us</a></li>
+					<li><a href="why-us">Why Us</a></li>
+					<li><a href="career">Jobs</a></li>
 					<li class="dropdown">
 						<a href="#" class="dropdown-toggle" data-toggle="dropdown">Services <i class="fa fa-angle-down"></i> </a>
 						<div class="dropdown-menu mega-menu custom-scrollbar" style="background-color: rgba(81, 40, 79, 0.5); border: 1px solid rgba(81, 40, 79, 0.5); border-radius: 20px">
 							<div class="row">
 								<div class="col-sm-4">
 									<img src="public/assets/images/catering/2705385-min.jpg" class="img-responsive">
-									<a href="service2.php">
+									<a href="catering">
 										<h4>CATERING</h4>
 									</a>
 									<img src="public/assets/images/light-industrial-staff/welder-673559.jpg" class="img-responsive">
-									<a href="service4.php">
+									<a href="light-industrial-staff">
 										<h4>Light Industrial Staff</h4>
 									</a>
 
 									<img src="public/assets/images/construction/Construction-Laborers.jpg" class="img-responsive">
-									<a href="service8.php">
+									<a href="construction">
 										<h4>Construction</h4>
 									</a>
 
 								</div>
 								<div class="col-sm-4">
 									<img src="public/assets/images/events/events2.jpg" alt="Events" class="img-responsive">
-									<a href="service9.php">
+									<a href="events-&-promotions">
 										<h4>Events & Promotions</h4>
 									</a>
 
 									<img src="public/assets/images/it-and-tech/tech1.jpg" class="img-responsive">
-									<a href="service10.php">
+									<a href="it-&-telecom">
 										<h4>IT & Telecom</h4>
 									</a>
 
 								</div>
 								<div class="col-sm-4">
 									<img src="public/assets/images/hospitality/hospitality1.jpg" class="img-responsive">
-									<a href="service12.php">
+									<a href="hospitality">
 										<h4>Hospitality</h4>
 									</a>
 									<img src="public/assets/images/sales-and-retail/sales1.jpg" class="img-responsive">
-									<a href="service11.php">
+									<a href="sales-&-retail">
 										<h4>Sales & Retail</h4>
 									</a>
 								</div>
@@ -188,17 +188,17 @@
 						<div class="dropdown-menu" style="">
 							<ul>
 
-								<li><a href="blog-item.php">How to achieve your bonus?</a></li>
+								<li><a href="blog-item">How to achieve your bonus?</a></li>
 							</ul>
 						</div>
 					</li>
-					<li><a href="testimonial.php">Testimonials</a></li>
+					<li><a href="testimonial">Testimonials</a></li>
 					<li>
-						<a href="location.php">Location</a>
+						<a href="location">Location</a>
 					</li>
-					<li><a href="contact.php">Contact</a></li>
+					<li><a href="contact">Contact</a></li>
 					<li>
-						<a href="book-a-temp.php">Book A Temp</a>
+						<a href="book-a-temp">Book A Temp</a>
 					</li>
 				</ul>
 				<!-- <ul class="nav navbar-nav navbar-right">
