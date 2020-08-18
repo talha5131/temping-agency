183dfb013049a3fb1a959f82227ac543ba0633d7
Muhammad Talha <talhamalik4147@gmail.com>
2020-08-18 20:28:05 +0500

    Aug 18,2020 8:28

--- resources/views/mail/job.blade.php
+++ resources/views/mail/job.blade.php
@@ -3,6 +3,14 @@
     <title></title>
 </head>
 <body>
+<h3>You Have Applied For This Job.</h3>
+<ul>
+    <li>Name: Hammad Ahmad</li><br>
+    <li>email: hammad-ahmad@gmail.com</li><br>
+    <!-- <li><img src="" alt=""></li> -->
+    <li>Phone Number: 0306-4629013</li><br>
+    <li>Your Password Is: 0123456789</li><br>
+</ul>
 <h1>{{ $user['name'] }}</h1>
 <p>{{ $user['info'] }}</p>
 <p>Thank you</p>
