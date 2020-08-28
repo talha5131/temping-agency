84853b5fc764c147134830848f270d61cc614025
Muhammad Talha <talhamalik4147@gmail.com>
2020-08-28 16:23:40 +0500

    Job Post Forms Updated

--- resources/views/admin/addJob.blade.php
+++ resources/views/admin/addJob.blade.php
@@ -171,9 +171,12 @@
                                     </div>
                                     
                                     <div class="col-sm-6" style="position:relative;">
-                                      <a class='btn btn-primary' href='javascript:;'>
+                                      <a class='btn btn-primary' href='#'>
                                         Choose File..
-                                        <input type="file" style='position:absolute;z-index:2;top:0;left:0;filter: alpha(opacity=0);-ms-filter:"progid:DXImageTransform.Microsoft.Alpha(Opacity=0)";opacity:0;background-color:transparent;color:transparent;' name="file_source" size="40"  onchange='$("#upload-file-info").html($(this).val());'>
+                                        <input type="file" style='position:absolute;z-index:2;top:0;left:0;filter: alpha(opacity=0);
+                                        -ms-filter:"progid:DXImageTransform.Microsoft.Alpha(Opacity=0)";opacity:0;
+                                        background-color:transparent;color:transparent;'
+                                        name="file_source" size="40"  onchange='$("#upload-file-info").html($(this).val());'>
                                       </a>
                                      &nbsp;
                                       <span class='label label-info' id="upload-file-info"></span>
--- resources/views/post-a-job.blade.php
+++ resources/views/post-a-job.blade.php
@@ -9,6 +9,10 @@
 			overflow-x: hidden;
 		}
 	}
+
+	.custom-radio {
+		margin-left: 10px;
+	}
 </style>
 
 
@@ -122,159 +126,173 @@
 		<div class="row about-wrapper-top">
 
 			<div class="col-md-12 ts-padding about-message">
-			<form id="postAJob" action="post-a-job" method="post">
-                        @csrf
-                        {{-- <div class="form-group">--}}
-                        {{-- <label for="">Have an account?</label>--}}
-                        {{-- <p>--}}
-                        {{-- If you don't have an account you can create one below by entering your email address/username. Your account details will be confirmed via email.--}}
-                        {{-- </p>--}}
-                        {{-- <button class="btn btn-success btn-lg">Sign In</button>--}}
-                        {{-- </div>--}}
-                        <hr>
-                        <div class="row">
-                            <div class="col-sm-12 form-group">
-                                <label for="jobTitle">Job Title</label>
-                                <input type="text" class="form-control" id="title" name="title" placeholder="Job Title">
-                            </div>
-                        </div>
-                        <hr>
-                        <div class="row">
-                            <div class="col-sm-12 form-group">
-                                <label for="desc"> Job Description</label>
-                                <textarea class="form-control job-content" rows="7" id="desc" name="desc"></textarea>
+				<form id="postAJob" action="post-a-job" method="post">
+					@csrf
+					{{-- <div class="form-group">--}}
+					{{-- <label for="">Have an account?</label>--}}
+					{{-- <p>--}}
+					{{-- If you don't have an account you can create one below by entering your email address/username. Your account details will be confirmed via email.--}}
+					{{-- </p>--}}
+					{{-- <button class="btn btn-success btn-lg">Sign In</button>--}}
+					{{-- </div>--}}
+					<hr>
+					<div class="row">
+						<div class="col-sm-12 form-group">
+							<label for="jobTitle">Job Title</label>
+							<input type="text" class="form-control" id="title" name="title" placeholder="Job Title">
+						</div>
+					</div>
+					<hr>
+					<div class="row">
+						<div class="col-sm-12 form-group">
+							<label for="desc"> Job Description</label>
+							<textarea class="form-control job-content" rows="7" id="desc" name="desc"></textarea>
 
-                            </div>
-                        </div>
-                        <hr>
-                        <div class="row">
-                            <div class="col-sm-4 form-group">
-                                <label for="tags">Job Tags</label>
-                                <input type="tags" data-role="tagsinput" class="form-control">
-                            </div>
-
-
-
-                            <div class="col-sm-4 form-group">
-                                <label for="jobtype">Job Category</label>
-                                <select class="form-control" id="category" name="category">
-                                    <option selected disabled>Please Select</option>
-                                    <option value="Trade & Construction">Trade & Construction</option>
-                                    <option value="Site Labourers & Cleaners">Site Labourers & Cleaners</option>
-                                    <option value="Multi-Skilled Builders">Multi-Skilled Builders</option>
-                                    <option value="Forklift Operatives">Forklift Operatives</option>
-                                    <option value="Warehouse">Warehouse</option>
-                                    <option value="Drivers">Drivers</option>
-                                    <option value="Pickers & Packers">Pickers & Packers</option>
-                                    <option value="Electricians & Electricians Mate">Electricians & Electricians Mate</option>
-                                    <option value="Plumbing">Plumbing</option>
-                                    <option value="Electrical">Electrical</option>
-                                    <option value="Cleaning">Cleaning</option>
-                                    <option value="Property Maintenance">Property Maintenance</option>
-                                </select>
-                            </div>
+						</div>
+					</div>
+					<hr>
+					<div class="row">
+						<div class="col-sm-4 form-group">
+							<label for="tags">Job Tags</label>
+							<input type="tags" data-role="tagsinput" class="form-control" id="tags" name="tags">
+						</div>
 
 
 
-                            <div class="col-sm-4 form-group">
-                                <label for="location">Location <small>(optional)</small></label>
-                                <input type="text" class="form-control" id="location" name="location" placeholder="e.g. London">
-                            </div>
-                        </div>
-                        <hr>
-                        <div class="row">
-                            <div class="col-sm-4 form-group">
-                                <label for="location">Duration</label>
-                                <input type="text" class="form-control" id="location" name="location" placeholder="Duration">
-                            </div>
+						<div class="col-sm-4 form-group">
+							<label for="jobtype">Job Category</label>
+							<select class="form-control" id="category" name="category">
+								<option selected disabled>Please Select</option>
+								<option value="Trade & Construction">Trade & Construction</option>
+								<option value="Site Labourers & Cleaners">Site Labourers & Cleaners</option>
+								<option value="Multi-Skilled Builders">Multi-Skilled Builders</option>
+								<option value="Forklift Operatives">Forklift Operatives</option>
+								<option value="Warehouse">Warehouse</option>
+								<option value="Drivers">Drivers</option>
+								<option value="Pickers & Packers">Pickers & Packers</option>
+								<option value="Electricians & Electricians Mate">Electricians & Electricians Mate</option>
+								<option value="Plumbing">Plumbing</option>
+								<option value="Electrical">Electrical</option>
+								<option value="Cleaning">Cleaning</option>
+								<option value="Property Maintenance">Property Maintenance</option>
+							</select>
+						</div>
 
-                            <div class="col-sm-4 form-group">
-                                <label for="location">Joining Date</label>
-                                <input type="text" class="form-control" id="location" name="location" placeholder="Joining Date">
-                            </div>
 
-                            <div class="col-sm-4 form-group">
-                                <label for="location">Ending Date</label>
-                                <input type="text" class="form-control" id="location" name="location" placeholder="Ending Date">
-                            </div>
-                        </div>
-                        <hr>
-                        <div class="row">
-                            <div class="col-sm-4 form-group">
-                                <label for="jobtype">Job Timing</label>
-                                <select class="form-control" id="jobtype" name="jobtype">
-                                    <option value="Freelance">Freelance</option>
-                                    <option value="Full Time" selected>Full Time</option>
-                                    <option value="Internship">Internship</option>
-                                    <option value="Part Time">Part Time</option>
-                                    <option value="Temporary">Temporary</option>
-                                </select>
-                            </div>
 
-                            <div class="col-sm-4 form-group">
-                                <label for="appEmail">Salary</label>
-                                <div class="row">
-                                    <label class="col-sm-2">From</label>
-                                    <input type="text" class="col-sm-4 form-control" id="appEmail" name="appEmail" placeholder="Enter an e-mail adress or website URL">
-                                    <label class="col-sm-2">To</label>
-                                    <input type="text" class="col-sm-4 form-control" id="appEmail" name="appEmail" placeholder="Enter an e-mail adress or website URL">
-                                </div>
-                            </div>
+						<div class="col-sm-4 form-group">
+							<label for="location">Location <small>(optional)</small></label>
+							<input type="text" class="form-control" id="location" name="location" placeholder="e.g. London">
+						</div>
+					</div>
+					<hr>
+					<div class="row">
+						<div class="col-sm-4 form-group">
+							<label for="location">Duration</label>
+							<input type="text" class="form-control" id="duration" name="duration" placeholder="Duration">
+						</div>
 
-                            <div class="col-sm-4 form-group">
-                                <label for="to" class="col-form-label">Salary Type</label>
-                                <div class="input-group">
-                                    <label class="custom-control custom-radio custom-control-inline">
-                                        <input type="radio" name="payType" checked="" value="per hour" class="custom-control-input"><span class="custom-control-label">Per Hour</span>
-                                    </label>
-                                    <label class="custom-control custom-radio custom-control-inline">
-                                        <input type="radio" name="payType" value="per week" class="custom-control-input"><span class="custom-control-label">Per Week</span>
-                                    </label>
-                                    <label class="custom-control custom-radio custom-control-inline">
-                                        <input type="radio" name="payType" value="per year" class="custom-control-input"><span class="custom-control-label">Per Year</span>
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-                        <hr>
-                        <div class="row">
-                            <div class="col-sm-4 form-group">
-                                <label for="tags">Job Purpose</label>
-                                <input type="text" data-role="tagsinput" class="form-control">
-                            </div>
-                            <div class="col-sm-4 form-group">
-                                <label for="compName">Job Responsibilites</label>
-                                <input type="text" class="form-control" id="company" name="company" placeholder="Enter the name of the company">
-                            </div>
-                            <div class="col-sm-4 form-group">
-                                <label for="website">Job Requirements</label>
-                                <input type="text" class="form-control" id="website" name="website" placeholder="http://">
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-sm-6 form-group">
-                                <div class="row">
-                                    <div class="col-md-2">
-                                        <label for="logo">Add Image</label>
-                                    </div>
-                                    
-                                    <div class="col-sm-6" style="position:relative;">
-                                      <a class='btn btn-primary' href='javascript:;'>
-                                        Choose File..
-                                        <input type="file" style='position:absolute;z-index:2;top:0;left:0;filter: alpha(opacity=0);-ms-filter:"progid:DXImageTransform.Microsoft.Alpha(Opacity=0)";opacity:0;background-color:transparent;color:transparent;' name="file_source" size="40"  onchange='$("#upload-file-info").html($(this).val());'>
-                                      </a>
-                                     &nbsp;
-                                      <span class='label label-info' id="upload-file-info"></span>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-sm-6 form-group">
-                                
-                                <button type="submit" class=" btn btn-primary" style="float:right; margin-left:10px;">Submit</button>                                
-                            </div>
-                        </div>
+						<div class="col-sm-4 form-group">
+							<label for="location">Joining Date</label>
+							<input type="text" class="form-control" id="joiningDate" name="joiningDate" placeholder="dd / mm / yyyy">
+						</div>
 
-                    </form>
+						<div class="col-sm-4 form-group">
+							<label for="location">Ending Date</label>
+							<input type="text" class="form-control" id="endingDate" name="endingDate" placeholder="dd / mm / yyyy">
+						</div>
+					</div>
+					<hr>
+					<div class="row">
+						<div class="col-sm-4 form-group">
+							<label for="jobtype">Job Timing</label>
+							<select class="form-control" id="jobTiming" name="jobTiming">
+								<option value="Freelance">Freelance</option>
+								<option value="Full Time" selected>Full Time</option>
+								<option value="Internship">Internship</option>
+								<option value="Part Time">Part Time</option>
+								<option value="Temporary">Temporary</option>
+							</select>
+						</div>
+
+						<div class="col-sm-4 form-group">
+							<label for="salary">Salary</label>
+							<div class="row">
+								<div class="col-md-6" style="padding: 0px !important;">
+									<div class="col-sm-4">
+										<label class="">From</label>
+									</div>
+									<div class="col-sm-8" style="padding: 0px !important;">
+										<input type="text" class="form-control" id="salaryFrom" name="salaryFrom" placeholder="From">
+									</div>
+								</div>
+								<div class="col-md-6" style="padding: 0px !important;">
+									<div class="col-sm-4" style="padding-right: 0px !important;">
+										<label class="">To</label>
+									</div>
+									<div class="col-sm-8" style="padding: 0px !important; margin-left: -25px;">
+										<input type="text" class="form-control" id="salaryTo" name="salaryTo" placeholder="To">
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div class="col-sm-4 form-group">
+							<label for="tsalaryType" class="col-form-label">Salary Type</label>
+							<div class="input-group">
+								<label class="custom-control custom-radio custom-control-inline">
+									<input type="radio" name="salaryType" checked="" value="per hour" class="custom-control-input"><span class="custom-control-label custom-radio"> Per Hour</span>
+								</label>
+								<label class="custom-control custom-radio custom-control-inline">
+									<input type="radio" name="salaryType" value="per week" class="custom-control-input"><span class="custom-control-label custom-radio"> Per Week</span>
+								</label>
+								<label class="custom-control custom-radio custom-control-inline">
+									<input type="radio" name="salaryType" value="per year" class="custom-control-input"><span class="custom-control-label custom-radio"> Per Year</span>
+								</label>
+							</div>
+						</div>
+					</div>
+					<hr>
+					<div class="row">
+						<div class="col-sm-4 form-group">
+							<label for="jobPurpose">Job Purpose</label>
+							<input type="text" name="jobPurpose" class="form-control" placeholder="Purpose">
+						</div>
+						<div class="col-sm-4 form-group">
+							<label for="responsibilities">Job Responsibilites</label>
+							<input type="text" class="form-control" id="responsibilities" name="responsibilities" placeholder="Responsibilities">
+						</div>
+						<div class="col-sm-4 form-group">
+							<label for="requirements">Job Requirements</label>
+							<input type="text" class="form-control" id="requirements" name="requirements" placeholder="Requirements">
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-6 form-group">
+							<div class="row">
+								<div class="col-md-3">
+									<label for="logo">Add Image</label>
+								</div>
+
+								<div class="col-md-9 style="position:relative;">
+									<a class='btn btn-primary'>
+										Choose File..
+										<input type="file" style='position:absolute;z-index:2;top:0;left:0;filter: alpha(opacity=0);
+										-ms-filter:"progid:DXImageTransform.Microsoft.Alpha(Opacity=0)";opacity:0;background-color:transparent;
+										color:transparent;' name="file_source" size="40" onchange='$("#upload-file-info").html($(this).val());'>
+									</a>
+									&nbsp;
+									<span class='label label-info' id="upload-file-info"></span>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-6 form-group">
+
+							<button type="submit" class=" btn btn-primary" style="float:right; margin-left:10px;">Submit</button>
+						</div>
+					</div>
+
+				</form>
 
 			</div>
 			<!--/ About image end -->
